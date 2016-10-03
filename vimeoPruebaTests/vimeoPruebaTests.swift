8af18677ce00e5d41cd3f7c1a3eb47abcef80904
Juan david Lopera lopez <juandavidloperalopez@iMac-de-Juan.local>
2016-10-03 08:12:43 -0500

    Initial Commit

---
+++ vimeoPruebaTests/vimeoPruebaTests.swift
@@ -0,0 +1,36 @@
+//
+//  vimeoPruebaTests.swift
+//  vimeoPruebaTests
+//
+//  Created by Juan david Lopera lopez on 3/10/16.
+//  Copyright © 2016 Juan david Lopera lopez. All rights reserved.
+//
+
+import XCTest
+@testable import vimeoPrueba
+
+class vimeoPruebaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
